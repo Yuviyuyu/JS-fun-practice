c283eaff99a975453e787c3e83c5c3ec91d7ef25
Yu <76136294+Yuviyuyu@users.noreply.github.com>
2022-11-05 15:55:16 +0930

    Create main.js

---
+++ main.js
@@ -0,0 +1,5 @@
+alert("Welcome to love calculator!");
+prompt("What is your lover's name?");
+prompt("What is your name?");
+var n = Math.floor(Math.random()*100);
+alert( "the sucess % of your love is " + n + " %!");
