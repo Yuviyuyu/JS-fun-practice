5900925af98aa03791f440b6557a840e25805125
Yu <76136294+Yuviyuyu@users.noreply.github.com>
2022-11-05 16:10:23 +0930

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
-alert("Welcome to love calculator!");
+alert("Welcome to Love Calculator!");
 prompt("What is your lover's name?");
 prompt("What is your name?");
-var n = Math.floor(Math.random()*100);
+
+var n = Math.floor(Math.random() * 100 + 1);
 alert( "the sucess % of your love is " + n + " %!");
+
+if (n<=40) {
+    alert("I promise, you are so much better off.");
+}
+else if (n > 40 && n <=80) {
+    alert("Maybe a weekend date is the beggning of everything?");
+}
+
+else{
+    alert("What are you waiting for, ask the beloved one out!");
+}
